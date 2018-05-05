a99dcd76157ba07bd914e168fba3e1f9ae717e1b
Armando <rivera.armando997@gmail.com>
2018-05-04 21:30:54 -0500

    Paginas login-contacto-opiniones

--- js/add.js
+++ js/add.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
  	$('.slider').slider();
  	$('.carousel').carousel();
+    $('.parallax').parallax();
  });
+
+function Ejecuta() {
+ 	alert("Mensaje enviado, Tus comentarios nos ayudan a seguir creciendo")
+ }
