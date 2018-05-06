e52a34444a5f81d867e22f4ad97d00a648e24ea7
AlicePeach <alicepeachmelocoton2@gmail.com>
2018-05-06 14:26:57 -0500

    respaldo

--- js/add.js
+++ js/add.js
@@ -3,21 +3,8 @@ $(document).ready(function(){
  	$('.slider').slider();
  	$('.carousel').carousel();
     $('.parallax').parallax();
-    $('.scrollspy').scrollSpy();
  });
 
 function Ejecuta() {
  	alert("Mensaje enviado, Tus comentarios nos ayudan a seguir creciendo")
  }
-<<<<<<< HEAD
-=======
-
- function Finalizar() {
- 	alert("Tu cita se ha registrado gracias por tu preferencia")
- }
-
- function Registro() {
- 	alert("Registro exitoso, Gracias por tu preferencia")
- 	document.location.href=index.html;
- }
->>>>>>> 540cadc88410d24250d4033445508208e8c28a28
