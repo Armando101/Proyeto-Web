551df9625a1b85db7a7d09000095298009b317d8
Armando <rivera.armando997@gmail.com>
2018-05-13 03:15:02 -0500

    Cambio de fondo y Servicios

--- js/add.js
+++ js/add.js
@@ -5,18 +5,8 @@ $(document).ready(function(){
     $('.carousel.carousel-slider').carousel({
     fullWidth: true
 	});
-<<<<<<< HEAD
-	$('.pushpin-demo-nav').each(function() {
-    var $this = $(this);
-    var $target = $('#' + $(this).attr('data-target'));
-    $this.pushpin({
-      top: $target.offset().top,
-      bottom: $target.offset().top + $target.outerHeight() - $this.height()
-    });
-=======
 	 $(document).ready(function(){
     $('.pushpin').pushpin();
->>>>>>> ebf8569807792dc22ec1c2820fd2164ba50b5157
   });
  });
 
